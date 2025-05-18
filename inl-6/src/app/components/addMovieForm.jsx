a19bfbb2b554739d2d71ad91746d140a54952b17
inessuhonjic <ines.suhonjic@hotmail.se>
2025-05-18 19:59:38 +0200

    följde en tutorial eller två och testade mig fram - obs mest testing

--- inl-6/src/app/components/addMovieForm.jsx
+++ inl-6/src/app/components/addMovieForm.jsx
@@ -1,20 +1,72 @@
-
-
-
 import React from "react";
-const AddMovie = ({ placeholder, onChange, value, onAddMovie }) => {
-  return(
-    
+
+const AddMovie = ({ title, rating, onAddMovie, onRatingChange, onTitleChange }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddMovie(); // Anropar funktionen som skickas via props
+  };
+  return (
+
+
     <div className="flex max-w-md flex-col gap-4">
-        <input type = "text" 
-        placeholder={placeholder}
-        onChange={onChange} 
-        value={value}/>
+      <form className="w-full max-w-sm" method="post" onSubmit={handleSubmit}>
+        <div className="md:flex md:items-center mb-1">
+          <div className="inline-block relative w-64">
+            <label className=" text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="titleField">
+              Titel:
+            </label>
+          </div>
+        </div>
+        <div className="md:w-2/3">
+          <input className="bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="titleField"
+            type="text"
+            placeholder="Titel.."
+            value={title}
+            onChange={onTitleChange}
+          ></input>
+        </div>
 
-        <button onClick={onAddMovie}>Lägg till</button>
+
+
+
+
+        <div className="inline-block relative w-64 mb-6">
+          <label className=" text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="ratingField">Betyg:</label>
+          <select className="bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            type="text"
+            id="ratingField"
+            value={rating}
+            onChange={onRatingChange}>
+            <option value="0">Välj betyg här...</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+          </div>
+        </div>
+
+
+
+
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+              Lägg till
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
-    
-  
-)}
-export default AddMovie;
 
+
+  )
+}
+
+
+export default AddMovie;
--- inl-6/src/app/page.js
+++ inl-6/src/app/page.js
@@ -4,12 +4,31 @@ import AddMovie from "../app/components/addMovieForm"
 import Movie from "../app/components/movie"
 
 export default function Home() {
-  const [movies, setMovies] = useState([]);
-  const [title, setTitle] = useState("");  
+const [movies, setMovies] = useState ([])
+const [title, setTitle] = useState ("")
+const [rating, setRating] = useState ("")
+ 
+
+
+  /*Funktion som anropas när man klickar på "Lägg till film" - se längre ner för anrop - väldigt osäker på syntax - men propsen onAddMovie skickar med själva funktionen:
+        <AddMovie 
+        title={title} -> props för titel och samma för rating under
+        rating={rating}
+        onTitleChange={(e) => setTitle(e.target.value)}  -> anonym funktion `? setTitle tar 'e.target.value' - hämtar värdet som matades in med event och ändrar värdet i titel till den nya som angavs
+        onRatingChange={(e) => setRating(e.target.value)} -> forts. onChange är en event handler (typ eventlistener?) som används för att fånga upp förändringar i formelement - skickas som props från komponenten
+       onAddMovie={handleAddMovie}/> -> triggar igång alltihopa eftersom Lägg till knappen trycktes på
+
+  */
 
   const handleAddMovie = () => {
-      setMovies([...movies, title]); // lägger till ny titel
+      const newMovie = {
+      title: title,
+      rating: rating
+    };
+      setMovies([...movies,newMovie]); // lägger till ny titel i movie - listan - men hur gör man för att lägga till rating stars också?!
+      
       setTitle(""); // nollställer inputfält
+      setRating(""); // nollställer ratingfält
     };
 
   return (
@@ -21,24 +40,27 @@ export default function Home() {
       
       
       <AddMovie 
-       placeholder="Skriv en titel"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        onAddMovie={handleAddMovie}/>
+        title={title}
+        rating={rating}
+        onTitleChange={(e) => setTitle(e.target.value)}
+        onRatingChange={(e) => setRating(e.target.value)}
+       onAddMovie={handleAddMovie}/> 
       <hr />
 
       <h2 className ="text font-bold mb-4">Filmer</h2> 
       <hr />
       <ul>
         
-       {movies.map((movie, index) => (
-    <Movie key={index} title={movie} />
-  ))}
+    {movies.map((movie, index) => (
+  <Movie key={index} title={movie.title} rating={movie.rating} />
+))}   
+    
+
       </ul>
     </div>
     
   );
-}
 
+}
 
 
