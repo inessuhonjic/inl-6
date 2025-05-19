65e3be48796510358fae83fae974a8a0d2c702f9
tobi-1337 <tobiasbjorklund1@gmail.com>
2025-05-19 15:02:36 +0200

    tror det funar

--- inl-6/src/app/components/MovieList.jsx
+++ inl-6/src/app/components/MovieList.jsx
@@ -5,18 +5,18 @@ const MovieList = ({ movies, onRemoveMovie }) => {
         {movies.map((movie, index) => (
     <li className="position-relative" key={index}>
       {movie.title}
-        <span>{movie.rating &&
+        <span className="">{movie.rating &&
           Array.from({ length: movie.rating }, (_, i) => (
             <img key={i} src="/images/star.png" alt="star"/>
           ))}
           </span>
 
-          <span><img src="/images/delete.png" 
+          <span className="position-absolute end-0"><img src="/images/delete.png" 
           alt="delete"
           onClick={() => onRemoveMovie(index)}
           /></span>
-      </p>
-    </div>
+      
+    </li>
     ))}
     </ul>
         
