9019ed55a9967a051c0dc6fdaaf7bc84ccf16a49
inessuhonjic <ines.suhonjic@hotmail.se>
2025-05-19 15:57:01 +0200

    lite styling

--- inl-6/src/app/components/MovieList.jsx
+++ inl-6/src/app/components/MovieList.jsx
@@ -3,18 +3,19 @@ const MovieList = ({ movies, onRemoveMovie }) => {
     return (
     <div>
         <h3 className="text-center">Inlagda Filmer!</h3>
-        <ul>
+        <ul className="list-group list-group-flush m-5">
         {movies.map((movie, index) => (
-    <li className="position-relative" key={index}>
+    <li className="position-relative list-group-item lead" key={index}>
       {movie.title}
-        <span className="">{movie.rating &&
+        <span className="m-3 img-fluid">{movie.rating &&
           Array.from({ length: movie.rating }, (_, i) => (
-            <img key={i} src="/images/star.png" alt="star"/>
+            <img key={i} src="/images/star.png" alt="star" id="star"/>
           ))}
           </span>
 
           <span className="position-absolute end-0"><img src="/images/delete.png" 
           alt="delete"
+          id="delete"
           onClick={() => onRemoveMovie(index)}
           /></span>
       
