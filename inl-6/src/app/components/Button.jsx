ece50ee1151ea19bb62863eba6d583c95cd0c52d
tobi-1337 <tobiasbjorklund1@gmail.com>
2025-05-19 16:14:02 +0200

    ändrade margin lite

--- inl-6/src/app/components/Button.jsx
+++ inl-6/src/app/components/Button.jsx
@@ -2,7 +2,7 @@
 //en Button-komponent med text och onClick som props. Återanvändbar.
 const Button = ({ text, onClick }) => {
   return (
-    <button className="btn btn-primary me-2 ms-4" onClick={onClick}>
+    <button className="btn btn-primary me-2" onClick={onClick}>
         {text}
     </button>
   );
